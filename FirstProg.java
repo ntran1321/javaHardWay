5d520df72b27d74bfe6bccd1f14b2cb1929184a8
Nancy Tran <nancy.tran.1321@gmail.com>
2017-03-16 14:51:21 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {	
+	public static void main( String [] args ) {
+		System.out.println( "My name is Nancy Tran." );
+		System.out.println( "Today's date is March 13, 2017" );
+	}
+}
