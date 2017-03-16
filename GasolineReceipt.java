be386a56ffe826389460293be0b83c3c0021c69f
Nancy Tran <nancy.tran.1321@gmail.com>
2017-03-16 14:58:13 -0600

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------------+");
+		System.out.println( "|                              |");
+		System.out.println( "|           CORNER STORE       |");
+		System.out.println( "|                              |");
+		System.out.println( "|     2016-03-30  04:20AM      |");
+		System.out.println( "|                              |");
+		System.out.println( "|   Gallons:       10.923      |");
+		System.out.println( "|   Price/gal:    $ 2.03       |");
+		System.out.println( "|                              |");
+		System.out.println( "|   Fuel total:   $22.71       |");
+		System.out.println( "|                              |");
+		System.out.println( "+------------------------------+");
+	}
+}
