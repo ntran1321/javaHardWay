eed221d490a709ae5a889ab94039f1c0e9864547
Nancy Tran <nancy.tran.1321@gmail.com>
2017-03-16 15:10:32 -0600

    exercise 4.2

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,7 @@
+public class PrintingChoicesEscapes {
+	public static void main( String[] args ) {
+		//This exercise demos escape sequences
+		System.out.println( "Learn\tJava\tthe\tHard\tWay\nHello\nSurprised? /* adasfdaf */ Or what did you expect?\nI hope you understand \"ESCAPE SEQUENCES\" now.\n"
+		);
+	}
+}
