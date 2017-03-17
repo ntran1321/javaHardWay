fc646b1bddcec0d7ec9352e039b0a044136d9ca3
Nancy Tran <nancy.tran.1321@gmail.com>
2017-03-16 22:02:54 -0600

    exercise 7

---
+++ ForgetfulMachine.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ForgetfulMachine {
+	public static void main( String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println( "What city is the capital of France?" );
+		keyboard.next();
+	}
+}
