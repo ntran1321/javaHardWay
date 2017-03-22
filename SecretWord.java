5996a6e5a096d641c195a52210b27018cc97c153
Nancy Tran <nancy.tran.1321@gmail.com>
2017-03-22 11:08:38 -0600

    exercise 19

---
+++ SecretWord.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SecretWord {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		String secret = "please", guess;
+
+		System.out.print( "What's the secret word? " );
+		guess = keyboard.next();
+
+		if ( guess.equals(secret) || guess.equals("mordor")) {
+			System.out.println( "That's correct!" );
+		}
+		else {
+			System.out.println( "No, the secret word isn't \"" + guess + "\".");
+		}
+
+		if ( guess == secret) {
+			System.out.println ( "This will never show.");
+		}
+		//this does not work beccause this case guess ==secret cannot be true
+	}
+}
